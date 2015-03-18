fa035c2c5d89b92923e239cf49d4b17a27500483
Raghav <raghav.kishan@gmail.com>
2015-03-19 00:41:00 +0530

    3nd commit

--- app/src/main/java/com/example/hp/finally1/MainActivity.java
+++ app/src/main/java/com/example/hp/finally1/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
